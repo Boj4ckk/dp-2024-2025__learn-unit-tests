672f797d8bfb3a301f90acc79f474264ff37f72f
yazkilito@gmail.com <yazkilito@gmail.com>
2025-03-22 12:54:41 +0100

    tp_1

--- src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
@@ -1,14 +1,35 @@
 package fr.anthonyquere.fizzbuzz;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzz {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        startFizzBuzz(2);
     }
 
     public static List<String> startFizzBuzz(int count) {
-        return null;
+        List<String> FizzBuzzList = new ArrayList<>();
+        for(int i = 1 ; i <= count ; i ++ ){
+            if(i % 3 == 0 && i % 5 != 0){
+                FizzBuzzList.add("Fizz");
+            }
+            else if(i % 5 == 0 && i % 3 != 0){
+                FizzBuzzList.add("Buzz");
+            }
+            else if(i % 5 != 0 && i % 3 != 0 ){
+                FizzBuzzList.add(String.valueOf(i));
+            }
+            else if(i % 3 == 0 && i % 5 == 0){
+                FizzBuzzList.add("FizzBuzz");
+            }
+
+        }
+
+
+
+        return FizzBuzzList;
+
     }
 }
